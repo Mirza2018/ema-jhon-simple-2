441961ab5174a317a8aa7e388c5a6f8c792d6b03
unknown <mirza.eee.4th@gmail.com>
2023-08-28 08:09:39 +0600

    final

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -3,16 +3,35 @@ import './Cart.css'
 
 
 const Cart = ({ cart }) => {
-    let sum = (a, b) => a + b.price;
-    let total = cart.reduce((sum), 0)
+
+
+    // let sum3 = (a, b) => a + b.quantity;
+    // let quantity = cart.reduce((sum3), 0)
+
+
+    let total = 0;
+    let quantity = 0;
+
+    for (const product of cart) {
+
+        product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity;
+        
+        quantity = quantity + product.quantity;
+    }
+
+    // let sum = (a, b) => a + b.price;
+    // let total = cart.reduce((sum), 0)
+
     let sum2 = (a, b) => a + b.shipping;
     let shipping = cart.reduce((sum2), 0)
-    const tax = total * 7 / 100;
+
+    const tax = total * 0.07;
     const allTotal = total + shipping + tax;
     return (
         <div className='cart'>
             <h3>Order summery</h3>
-            <p>Selected items: {cart.length}</p>
+            <p>Selected items: {quantity}</p>
             <p>Total Price:${total} </p>
             <p>Total Shipping:${shipping} </p>
             <p>Tax:${tax.toFixed(2)} </p>
